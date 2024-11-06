8b95b7abf8763dc3c9087860de7fa9baeca660b5
camilomartinezfo <caemartinezfo@unal.edu.co>
2024-11-06 08:55:56 -0500

    actualización

--- instrucciones.sh
+++ instrucciones.sh
@@ -4,20 +4,23 @@ echo "Inicio de ejecución de instrucciones"
 cd ..
 cd DeforestationData-back
 
-# Falta ejecutar los scripts Selenium para descarga de datos IDEAM
-
+# Rscript Scripts/IDEAM_dataSelenium.R
+# Rscript Scripts/IDEAM_data_processing.R
 
 # Rscript Scripts/GFW_data.R
-
 # Rscript Scripts/GFW_data_matchIDEAM.R
 
 # quarto render Reporte_pdf_dash.qmd
-
 # Rscript Scripts/html_to_pdf_report.R
 
 cd ..
 cd Dashboard-webpage
+
 # echo -e "\n" | quarto publish quarto-pub
+
+git add .
+git commit -m "Actualización datos reporte back"
+git push origin main
 
 
 cd ..
